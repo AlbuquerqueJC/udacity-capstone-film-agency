a46dbe68af2e153e67479dc4a15dc7ebedcb9336
AlbuquerqueJC <john.albuquerque@gmail.com>
2020-06-08 16:00:35 -0400

    [Fix] Database conf

--- models.py
+++ models.py
@@ -24,8 +24,6 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.drop_all()
-    db.create_all()
 
 
 def db_drop_and_create_all():
