2199b3f6a3844b9bf0f9ba549d431d625ff90a8d
AlbuquerqueJC <john.albuquerque@gmail.com>
2020-06-08 15:27:45 -0400

    [Fix] Database configuration and formatting

--- app.py
+++ app.py
@@ -4,26 +4,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  @app.route('/')
-  def get_greeting():
-      # excited = 'false'
-      excited = os.environ['EXCITED']
-      greeting = "Hello"
-      if excited == 'true':
-          greeting = greeting + "!!!!!"
-      return greeting
+    @app.route('/')
+    def get_greeting():
+        # excited = 'false'
+        excited = os.environ['EXCITED']
+        greeting = "Hello"
+        if excited == 'true':
+            greeting = greeting + "!!!!!"
+        return greeting
 
-  @app.route('/coolkids')
-  def be_cool():
-      return "Be cool, man, be coooool! You're almost a FSND grad!"
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
 
-  return app
+    return app
+
 
 APP = create_app()
 
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import os
+import this
+
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -15,11 +17,19 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.drop_all()
+    db.create_all()
+
+
+def db_drop_and_create_all():
+    db.drop_all()
     db.create_all()
 
 
@@ -27,19 +37,22 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
+
+
 class Person(db.Model):
-  __tablename__ = 'People'
+    __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def __init__(self, name, catchphrase=""):
+        self.name = name
+        self.catchphrase = catchphrase
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    def format(self):
+        return {
+            'id'         : self.id,
+            'name'       : self.name,
+            'catchphrase': self.catchphrase
+        }
